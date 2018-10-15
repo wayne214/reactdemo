37b4dbeb943b4f8bf7b6dd741ce1ea5a89870dbb
wayne <jun_wei_like1@163.com>
2018-10-15 09:44:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <list></list>
           <a
             className="App-link"
             href="https://reactjs.org"
